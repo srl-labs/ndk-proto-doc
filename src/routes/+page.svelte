b91e07ba1ecedb80e05bd2ebea374df37f76cc3d
Siva Sivakumar <siva.sivakumar@nokia.com>
2023-12-15 22:27:07 +0100

    home page dark theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,17 +50,17 @@
 					</p>
 				</div>
 			</div>
-			<div class="mt-5 bg-white rounded-lg shadow-xl">
-				<div class="my-1.5 h-[305px] overflow-y-auto scroll-light">
+			<div class="mt-5 bg-white dark:bg-gray-800 rounded-lg shadow-xl">
+				<div class="my-1.5 h-[305px] overflow-y-auto scroll-light dark:scroll-dark">
 					<ul>
 						{#each panelList as item, i}
 							<li
-								class="items-center p-4 text-gray-900 hover:bg-gray-100 {i > 0 ? 'border-t' : ''}"
+								class="items-center p-4 text-gray-900 hover:bg-gray-100 {i > 0 ? 'border-t dark:border-gray-700' : ''} dark:hover:bg-gray-700"
 							>
 								<a data-sveltekit-reload href={item.href}>
-									<p class="">{item.name}</p>
+									<p class="dark:text-white">{item.name}</p>
 									{#if item.desc}
-										<p class="mt-1 font-light text-sm">{item.desc}</p>
+										<p class="mt-1 font-light text-sm dark:text-gray-400">{item.desc}</p>
 									{/if}
 								</a>
 							</li>
